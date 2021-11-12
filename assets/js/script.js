1ce06ddb12a73de63bd76d6cf029a00bfaf27d3d
Orion Szathmary <orion@victordirect.com>
2021-11-12 10:59:49 -0500

    default browser event turned off

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is the new task";
     tasksToDoEl.appendChild(taskItemEl);
+    console.log(event);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
