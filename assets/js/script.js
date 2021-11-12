b2d83ee00836c973fb3c5563b095fe55ffd6487b
Orion Szathmary <orion@victordirect.com>
2021-11-12 14:25:28 -0500

    Validation and form reset added

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,19 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if(!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
+    
     // package data as an object
     var taskDataObject = {
         name: taskNameInput,
         type: taskTypeInput
     };
-
+    
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObject);
 };
@@ -24,8 +31,7 @@ var createTaskEl = function(taskDataObject) {
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObject.name + "</h3><span class='task-type'>" + taskDataObject.type + "</span>";
-    
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObject.name + "</h3><span class='task-type'>" + taskDataObject.type + "</span>";  
     listItemEl.appendChild(taskInfoEl);
     
     // add entire list item to list
